2ca04ed14132db9dd4e51a7f5c8c57dadbc42d40
KevinLZC <klazaro0@ucol.mx>
2023-04-16 01:35:16 -0600

    Página colaboradores terminada

--- app/colaboradores/page.jsx
+++ app/colaboradores/page.jsx
@@ -6,27 +6,31 @@ import User from '../../public/user.png'
 export default function Colaboradores () {
   return (
     <>
-      <div className='flex space-x-[75px] w-full'>
-        <div className='rounded-2xl mt-10 flex flex-col items-center w-80 h-80 shadow-2xl'>
-          <Image className='w-60 rounded-full translate-y-2' alt='Leonel' src={User} />
-          <span className='translate-y-4'>Leonel Alejandro Ceballos Alvarado</span>
-          <span className='text-sm translate-y-4'>Co-fundador de CLG</span>
+      <div className='flex flex-col mb-4'>
+        <div className='lg:flex lg:mr-[450px] lg:ml-[450px] md:gap-10'>
+          <div className='rounded-2xl min-w-[320px] mt-10 w-80 md:w-80 lg:w-80 flex flex-col items-center shadow-2xl mr-auto ml-auto'>
+            <Image className='w-60 rounded-full' alt='Leonel' src={User} />
+            <span>Leonel Alejandro Ceballos Alvarado</span>
+            <span className='text-sm'>Co-fundador de CLG</span>
+          </div>
+          <div className='rounded-2xl mt-10 min-w-[320px] w-80 flex flex-col items-center shadow-2xl mr-auto ml-auto'>
+            <Image className='w-60 mt-2 mb-2 rounded-full' alt='Angeles' src={Angeles} />
+            <span>María de los Ángeles Gutiérrez Barajas</span>
+            <span className='text-sm'>Co-fundadora de CLG</span>
+          </div>
         </div>
-        <div className='rounded-2xl mt-10 flex flex-col items-center w-80 h-80 shadow-2xl'>
-          <Image className='w-60 rounded-full translate-y-2' alt='Angeles' src={Angeles} />
-          <span className='translate-y-4'>María de los Ángeles Gutiérrez Barajas</span>
-          <span className='text-sm translate-y-4'>Co-fundadora de CLG</span>
+        <div className='lg:flex lg:mr-[450px] lg:ml-[450px] md:gap-10'>
+          <div className='rounded-2xl mt-10 min-w-[320px] w-80 flex flex-col items-center shadow-2xl mr-auto ml-auto'>
+            <Image className='w-60 rounded-full' alt='Kevin' src={User} />
+            <span>Kevin Lázaro Cernas</span>
+            <span className='text-sm'>Co-fundador de CLG</span>
+          </div>
+          <div className='rounded-2xl mt-10 min-w-[320px] w-80 flex flex-col items-center shadow-2xl mr-auto ml-auto'>
+            <Image className='w-60 mt-2 mb-2 rounded-full' alt='Cecilia' src={Cecilia} />
+            <span>Cecilia López Martínez</span>
+            <span className='text-sm'>Co-fundadora de CLG</span>
+          </div>
         </div>
-        <div className='rounded-2xl mt-10 flex flex-col items-center w-80 h-80 shadow-2xl'>
-          <Image className='w-60 translate-y-2' alt='Kevin' src={User} />
-          <span className='translate-y-4'>Kevin Lázaro Cernas</span>
-          <span className='text-sm translate-y-4'>Co-fundador de CLG</span>
-        </div>
-      </div>
-      <div className='rounded-2xl mt-10 flex flex-col items-center w-80 h-80 translate-x-[395px] shadow-2xl'>
-        <Image className='w-60 rounded-full translate-y-2' alt='Cecilia' src={Cecilia} />
-        <span className='translate-y-4'>Cecilia López Martínez</span>
-        <span className='text-sm translate-y-4'>Co-fundadora de CLG</span>
       </div>
     </>
   )
